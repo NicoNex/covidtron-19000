6f000bacc2127555d10dd1de6c1e97985eb1162b
Dj_Mike238 <mike238dimaggio@gmail.com>
2020-03-26 18:39:30 +0100

    main.go: Implement new commands and edit /start

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 
 	"github.com/NicoNex/covidtron-19000/cache"
+	"github.com/NicoNex/covidtron-19000/c19"
 	"github.com/NicoNex/echotron"
 )
 
@@ -17,6 +18,16 @@ type bot struct {
 
 var cc *cache.Cache
 
+type botStates int
+
+const (
+	idle = iota
+	regione
+	provincia
+)
+
+var botState = botStates.idle
+
 func NewBot(engine echotron.Api, chatId int64) echotron.Bot {
 	go cc.SaveSession(chatId)
 
@@ -27,9 +38,63 @@ func NewBot(engine echotron.Api, chatId int64) echotron.Bot {
 }
 
 func (b *bot) Update(update *echotron.Update) {
-	if update.Message.Text == "/start" {
-		b.SendMessage("Hello world", b.chatId)
+	switch botState {
+	case botStates.idle:
+		if update.Message.Text == "/start" {
+			sendIntroduction()
+
+		} else if update.Message.Text == "/andamento" {
+			b.SendMessageOptions(c19.GetAndamentoMsg(), b.chatId, echotron.PARSE_MARKDOWN)
+
+		} else if update.Message.Text == "/regione" {
+			b.SendMessage("Inserisci il nome di una regione.", b.chatId)
+			botState = botStates.regione
+
+		} else if update.Message.Text == "/provincia" {
+			b.SendMessage("Inserisci il nome di una provincia.", b.chatId)
+			botState = botStates.provincia
+		}
+
+	case botStates.regione:
+		if update.Message.Text == "/cancel" {
+			b.SendMessage("Operazione annullata.", b.chatId)
+
+		} else {
+			b.SendMessageOptions(c19.GetRegioneMsg(update.Message.Text), b.chatId, echotron.PARSE_MARKDOWN)
+		}
+
+		botState = botStates.idle
+
+	case botStates.provincia:
+		if update.Message.Text == "/cancel" {
+			b.SendMessage("Operazione annullata.", b.chatId)
+
+		} else {
+			b.SendMessageOptions(c19.GetProvinciaMsg(update.Message.Text), b.chatId, echotron.PARSE_MARKDOWN)
+		}
+
+		botState = botStates.idle
 	}
+}
+
+func sendIntroduction() {
+	msg := `
+	*Benvenuto in Covidtron-19000!*
+
+	*Comandi:*
+	/start: visualizza questo messaggio
+	/andamento: visualizza andamento nazionale
+	/regione: visualizza andamento regione
+	/provincia: visualizza andamento provincia
+	/cancel: annulla l'operazione in corso
+
+	Bot creato da @NicoNex e @Dj_Mike238.
+	Basato su [echotron](https://github.com/NicoNex/echotron).
+
+	Icona creata da [Nhor Phai](https://www.flaticon.com/authors/nhor-phai) su [Flaticon](https://www.flaticon.com)
+	`
+	
+	b.SendMessageOptions(msg, b.chatId, echotron.PARSE_MARKDOWN | echotron.DISABLE_WEB_PAGE_PREVIEW)
 }
 
 func main() {
