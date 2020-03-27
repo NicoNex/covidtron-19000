f7a72cbbf0d01724392d5a5996de5dc0ed437a16
Dj_Mike238 <mike238dimaggio@gmail.com>
2020-03-28 00:06:46 +0100

    dataroutine.go: simplify things

--- dataroutine.go
+++ dataroutine.go
@@ -1,24 +1,13 @@
 package main
 
 import (
+	"github.com/jasonlvhit/gocron"
 	"github.com/NicoNex/covidtron-19000/c19"
-	"time"
 )
 
 func updateData() {
-	var timestamp int64
-
 	c19.Update()
-	timestamp = time.Now().Unix()
-
-	for {
-		t := time.Now().Unix()
-
-		if t-timestamp > 86400 {
-			c19.Update()
-			timestamp = t
-		}
-
-		time.Sleep(time.Hour)
-	}
+	gocron.Every(1).Day().At("18:30").Do(c19.Update)
+	
+	<- gocron.Start()
 }
