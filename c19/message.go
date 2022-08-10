837c5e042e6b92a84a926d4ce0509493214b4c06
Dj_Mike238 <22237379+DjMike238@users.noreply.github.com>
2022-08-10 19:03:17 +0200

    c19: move methods in utils.go

--- c19/message.go
+++ c19/message.go
@@ -20,7 +20,6 @@ package c19
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/NicoNex/covidtron-19000/apiutil"
 )
@@ -216,58 +215,4 @@ Totale positivi: *%s*`,
 	} else {
 		return "Errore: Provincia non trovata."
 	}
-}
-
-func formatNote(nota string, ntype NoteType) string {
-	var msg strings.Builder
-
-	msg.WriteString("\n\n*Note")
-
-	switch ntype {
-	case Note:
-		msg.WriteString(" generali")
-	case NoteCasi:
-		msg.WriteString(" relative ai test effettuati")
-	case NoteTest:
-		msg.WriteString(" relative ai casi testati")
-	}
-
-	msg.WriteString(":*")
-
-	note := strings.Split(nota, ". ")
-
-	for i, n := range note {
-		n = strings.TrimSuffix(n, "  ")
-
-		if !strings.HasSuffix(n, ".") {
-			n += "."
-		}
-
-		if strings.Contains(n, "  -") {
-			spl := strings.Split(n, "  -")
-
-			for _, s := range spl {
-				if strings.HasPrefix(s, " ") {
-					s = "-" + s
-				}
-
-				msg.WriteString(fmt.Sprintf("\n%s", s))
-			}
-		} else if strings.TrimSpace(n) != "." {
-			if i == 0 || (i > 0 && len(note[i-1]) != 6) {
-				msg.WriteString(fmt.Sprintf("\n- %s", n))
-			} else {
-				msg.WriteString(fmt.Sprintf(" %s", n))
-			}
-		}
-	}
-
-	return msg.String()
-}
-
-func plus(value int) string {
-	if value > 0 {
-		return "+" + apiutil.Ifmt(value)
-	}
-	return apiutil.Ifmt(value)
 }
